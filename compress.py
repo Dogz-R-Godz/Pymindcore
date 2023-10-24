f400c83a5f0ce5ac74e4bab0177f814c853aa8e5
Dogz-R-Godz <129635586+Dogz-R-Godz@users.noreply.github.com>
2023-10-24 18:00:14 +1100

    Update compress.py

--- compress.py
+++ compress.py
@@ -4,8 +4,13 @@ import time
 import numpy
 import imageio.v3
 from PIL import Image
-import screenshot as sc
+import screenshot2 as sc
 from discord_webhook import DiscordWebhook as DW
+import keyboard
+import os
+import pickle
+import json
+
 def compress_img(img, img_res=25, save_old = False):
     x,y,v = img.shape
     img = img.tolist()
@@ -50,7 +55,7 @@ def screenshot(img_res=25, save_old = False):
         full_img.append(row)
     arr = numpy.array(full_img)
     return(arr)
-def read_from_bmp(file:str):
+def read_from_bmp(file:str=r'C:\Users\brolf\Desktop\vscodecodefold\AI-framework-main\pymind v1\out.bmp'):
     img = imageio.v3.imread(file)
     return(img)
 def make_bmp(arr,filename,size):
@@ -78,12 +83,28 @@ def make_grayscale(arr,filename,size):
             count2 += 1
         count1 += 1
     I.save(f'{filename}.bmp')
-def numpy_com_optimised(arr,x,y):
+def numpy_com_optimised(arr):
     rgb_arrs = arr.transpose((2,0,1))
-    new_arrs = rgb_arrs.reshape(3,x,y,-1)
+    new_arrs = rgb_arrs.reshape(3,40,40,-1)
     averages = numpy.uint8(numpy.average(new_arrs, 3))
     averages = averages.transpose(2,1,0)
     return averages
+def one_liner(arr, shape=(30,36,30,64,3)):
+    return numpy.mean(arr.reshape(*shape).transpose(1,3,0,2,4).reshape(-1,shape[0],shape[2],shape[4]), 0).astype(int).transpose(1,0,2)
+def cf_optimised(final_res, starting_res, windowname, file=None, capture_rect=False, starting_point=(0,0)):
+    if type(starting_res) == int:
+        starting_res = (starting_res,starting_res)
+    if type(final_res) == int:
+        final_res = (final_res, final_res)
+    if starting_res[0] % final_res[0] != 0 or starting_res[1] % final_res[1] != 0:
+        raise Exception("final res must be a factor of starting res")
+    sc.take_bmp_screenshot(starting_res, 'uncompressed', windowname, capture_rect, starting_point)
+    img_arr = read_from_bmp('uncompressed.bmp')
+    shape = (final_res[1], starting_res[1] // final_res[1], final_res[0], starting_res[0] // final_res[0], 3)
+    img2 = one_liner(img_arr, shape)
+    if file != None:
+        make_bmp(img2, file, final_res)
+    return(img2)
 def numpy_compress(arr,gsc=True,size=25):
     if type(size) == type(1):
         x = size
@@ -117,8 +138,8 @@ def numpy_compress(arr,gsc=True,size=25):
             gray_sc.append(row)
         gray_sc = numpy.array(gray_sc)
     return((final_arr, gray_sc))
-def win_optomised(final_resolution, starting_resolution, windowname, file=None, grayscale=False):
-    sc.take_bmp_screenshot(starting_resolution, 'uncompressed', windowname)
+def win_optomised(final_resolution, starting_resolution, windowname, file=None, grayscale=False,capture_rect=False,starting_point=(0,0)):
+    sc.take_bmp_screenshot(starting_resolution, 'uncompressed', windowname,capture_rect,starting_point)
     img_arr = read_from_bmp('uncompressed.bmp')
     img2,gray_img = numpy_compress(img_arr,grayscale,final_resolution)
     if file != None:
@@ -154,20 +175,6 @@ def get_times():
     start = time.perf_counter()
     make_grayscale(gray_img, 'zNp_gray_bmp',25)
     print('time to save gray img:', time.perf_counter()-start)
-def compress_image(x,y):
-    start = time.perf_counter()
-    a = sc.take_bmp_screenshot(name='out')
-    #print('time to screenshot with win32:', time.perf_counter()-start)
-    start = time.perf_counter()
-    img_arr = read_from_bmp('out.bmp')
-    #print('time to read img:', time.perf_counter()-start)
-    #diff = abs(abs(img_arr)-abs(a))
-    #s = numpy.sum(diff)
-    start = time.perf_counter()
-    img = numpy_com_optimised(img_arr,x,y)
-    #print('time to fast numpy compress:', time.perf_counter()-start)
-
-    return img
 def send_to_discord(filenames, sendnames):
     webhook = DW("https://discord.com/api/webhooks/1125017628566618182/DVn9UbvA_cHlpk5-Jh1wj-WCODsaH30VS-cY-RqKWwvl4s5RsMRfgUni9Pb4SkT5NGzo")
     if type(filenames) == type(''):
@@ -186,13 +193,70 @@ def pil_bmp_to_jpg(filenames,outputnames):
             Image.open(filenames[index]).save(outputnames[index])
 #p
 if __name__ == '__main__':
+    ret_list = []
+    update = False
+    def logger(args):
+        global update, ret_list
+        update = True
+        ret_list.append([args.name, args.event_type, args.time])
+        return
+    keyboard.hook(logger)
     count = 0
-    #keyboard.wait('p')
-    #time.sleep(10)
-    start = time.perf_counter()
-    get_times()
-    print(time.perf_counter()-start)
-    count += 1
+    cwd = os.getcwd()
+    a = len(os.listdir(f"{cwd}\\screenies"))
+    os.mkdir(f"screenies/iteration_{a}")
+    count = 0
+    istrue = False
+    check = True
+    with open(r"C:\Users\brolf\AppData\Roaming\.minecraft\config\ChatTriggers\modules\pymindLinker\rot_info.json", 'r') as f:
+        string = f.read()
+        try:
+            last_rot = json.loads(string)
+        except:
+            a=1
+    while True:
+        if istrue:
+            cf_optimised(40,(1920,1080), 'Minecraft 1.8.9', f"screenies/iteration_{a}/Cf_test_bmp_{count}")
+            with open(r"C:\Users\brolf\AppData\Roaming\.minecraft\config\ChatTriggers\modules\pymindLinker\info_2.json", 'r') as f:
+                string = f.read()
+                if string != '':
+                    all_pos = json.loads(string)
+            with open(r"C:\Users\brolf\AppData\Roaming\.minecraft\config\ChatTriggers\modules\pymindLinker\rot_info.json", 'r') as f:
+                string = f.read()
+                if string != '':
+                    rot = json.loads(string)
+                    c_hange_in_rot = [rot[0]-last_rot[0], rot[1]-last_rot[1]]
+                    last_rot = rot
+            with open('current_keys.PICKLE', 'rb') as f:
+                keys = pickle.load(f)
+            savelist = [all_pos, c_hange_in_rot, keys]
+            with open(f"screenies/iteration_{a}/extra_info_{count}.PKL", 'wb') as f:
+                pickle.dump(savelist, f)
+            #sc.take_bmp_screenshot((1920, 1080), f"screenies/iteration_{a}/Cf_test_bmp_{count}", 'Minecraft 1.8.9')
+            count += 1
+            for x in ret_list:
+                if x[0] == 'p':
+                    if check:
+                        if x[1] == 'down':
+                            print('ending')
+                            istrue = False
+                            check = False
+                            exit('ended :)')
+                    if x[1] == 'up':
+                        check = True
+            ret_list = []
+        else:
+            time.sleep(0.025)
+            for x in ret_list:
+                if x[0] == 'p':
+                    if check:
+                        if x[1] == 'down':
+                            print('starting')
+                            istrue = True
+                            check = False
+                    if x[1] == 'up':
+                        check = True
+            ret_list = []
 """
 times = []
 for x in range(25):
